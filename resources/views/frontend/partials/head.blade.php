2fc1d8560ea60512b41a0b82930705c4b133bdb9
Salman Abbas (سلیمان) <55852619+ItsSalman99@users.noreply.github.com>
2022-11-27 00:44:41 -0800

    theme intialized

---
+++ resources/views/frontend/partials/head.blade.php
@@ -0,0 +1,32 @@
+<head>
+    <!-- ========== Meta Tags ========== -->
+    <meta charset="utf-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+    <meta name="description" content="Examin - Education and LMS Template">
+
+    <!-- ========== Page Title ========== -->
+    <title>Examin - Education and LMS Template</title>
+
+    <!-- ========== Favicon Icon ========== -->
+    <link rel="shortcut icon" href="assets/img/favicon.png" type="image/x-icon">
+
+    <!-- ========== Start Stylesheet ========== -->
+    <link href="{{ asset('assets/frontend/css/bootstrap.min.css') }}" rel="stylesheet" />
+    <link href="{{ asset('assets/frontend/css/font-awesome.min.css') }}" rel="stylesheet" />
+    <link href="{{ asset('assets/frontend/css/flaticon-set.css') }}" rel="stylesheet" />
+    <link href="{{ asset('assets/frontend/css/elegant-icons.css') }}" rel="stylesheet" />
+    <link href="{{ asset('assets/frontend/css/magnific-popup.css') }}" rel="stylesheet" />
+    <link href="{{ asset('assets/frontend/css/owl.carousel.min.css') }}" rel="stylesheet" />
+    <link href="{{ asset('assets/frontend/css/owl.theme.default.min.css') }}" rel="stylesheet" />
+    <link href="{{ asset('assets/frontend/css/animate.css') }}" rel="stylesheet" />
+    <link href="{{ asset('assets/frontend/css/bootsnav.css') }}" rel="stylesheet" />
+    <link href="{{ asset('assets/frontend/css/style.css') }}" rel="stylesheet">
+    <link href="{{ asset('assets/frontend/css/responsive.css') }}" rel="stylesheet" />
+    <!-- ========== End Stylesheet ========== -->
+
+    <!-- ========== Google Fonts ========== -->
+    <link href="https://fonts.googleapis.com/css?family=Open+Sans" rel="stylesheet">
+    <link href="https://fonts.googleapis.com/css?family=Poppins:400,500,600,700,800" rel="stylesheet">
+
+</head>
---
+++ resources/views/frontend/partials/header.blade.php
@@ -0,0 +1,187 @@
+<!-- Start Header Top
+    ============================================= -->
+<div class="top-bar-area address-one-lines bg-dark text-light">
+    <div class="container">
+        <div class="row">
+            <div class="col-md-10 address-info">
+                <div class="info box">
+                    <ul>
+                        <li>
+                            Have any question? +123 456 7890
+                        </li>
+                        <li>
+                            <i class="fas fa-envelope-open"></i>
+                            Info@gmail.com
+                        </li>
+                        <li>
+                            <i class="fas fa-clock"></i>
+                            Sun - Thu <span>8:00 - 16:00</span>
+                        </li>
+                    </ul>
+                </div>
+            </div>
+            <div class="user-login text-right col-md-2">
+                <a href="#">Apply Now</a>
+            </div>
+        </div>
+    </div>
+</div>
+<!-- End Header Top -->
+
+<!-- Header
+    ============================================= -->
+<header id="home">
+
+    <!-- Start Navigation -->
+    <nav class="navbar navbar-default navbar-sticky bootsnav">
+
+        <div class="container">
+
+            <!-- Start Header Navigation -->
+            <div class="navbar-header">
+                <button type="button" class="navbar-toggle" data-toggle="collapse" data-target="#navbar-menu">
+                    <i class="fa fa-bars"></i>
+                </button>
+                <a class="navbar-brand" href="index.html">
+                    {{-- <img src="{{ asset('assets/frontend/img/logo.png') }}" class="logo" alt="Logo"> --}}
+                    THE NEST ACADEMY
+                </a>
+            </div>
+            <!-- End Header Navigation -->
+
+            <!-- Collect the nav links, forms, and other content for toggling -->
+            <div class="collapse navbar-collapse" id="navbar-menu">
+                <ul class="nav navbar-nav navbar-right" data-in="#" data-out="#">
+                    <li class="dropdown">
+                        <a href="#" class="dropdown-toggle active" data-toggle="dropdown">Home</a>
+                        <ul class="dropdown-menu">
+                            <li class="dropdown">
+                                <a href="#" class="dropdown-toggle" data-toggle="dropdown">Multipage</a>
+                                <ul class="dropdown-menu">
+                                    <li><a href="index.html">Home Version One</a></li>
+                                    <li><a href="index-2.html">Home Version Two</a></li>
+                                    <li><a href="index-3.html">Home Version Three</a></li>
+                                    <li><a href="index-4.html">Home Version Four</a></li>
+                                    <li><a href="index-5.html">Home Version Five</a></li>
+                                    <li><a href="index-6.html">Home Version Six</a></li>
+                                    <li><a href="index-7.html">Home Version Serven</a></li>
+                                    <li><a href="index-8.html">Home Version Eight</a></li>
+                                </ul>
+                            </li>
+                            <li class="dropdown">
+                                <a href="#" class="dropdown-toggle" data-toggle="dropdown">Onepage</a>
+                                <ul class="dropdown-menu">
+                                    <li><a href="index-onepage.html">Home Version One</a></li>
+                                    <li><a href="index-2-onepage.html">Home Version Two</a></li>
+                                    <li><a href="index-3-onepage.html">Home Version Three</a></li>
+                                    <li><a href="index-4-onepage.html">Home Version Four</a></li>
+                                    <li><a href="index-5-onepage.html">Home Version Five</a></li>
+                                    <li><a href="index-6-onepage.html">Home Version Six</a></li>
+                                    <li><a href="index-7-onepage.html">Home Version Seven</a></li>
+                                    <li><a href="index-8-onepage.html">Home Version Eight</a></li>
+                                </ul>
+                            </li>
+                        </ul>
+                    </li>
+                    <li class="dropdown megamenu-fw">
+                        <a href="#" class="dropdown-toggle" data-toggle="dropdown">Pages</a>
+                        <ul class="dropdown-menu megamenu-content" role="menu">
+                            <li>
+                                <div class="row">
+                                    <div class="col-menu col-md-3">
+                                        <h6 class="title">Gallery</h6>
+                                        <div class="content">
+                                            <ul class="menu-col">
+                                                <li><a href="gallery-2-colum.html">Gallery Two Colum</a></li>
+                                                <li><a href="gallery-3-colum.html">Gallery Three Colum</a></li>
+                                                <li><a href="gallery-4-colum.html">Gallery Four Colum</a></li>
+                                                <li><a href="gallery-6-colum.html">Gallery Six Colum</a></li>
+                                            </ul>
+                                        </div>
+                                    </div><!-- end col-3 -->
+                                    <div class="col-menu col-md-3">
+                                        <h6 class="title">Advisor</h6>
+                                        <div class="content">
+                                            <ul class="menu-col">
+                                                <li><a href="advisor-carousel.html">Advisor Carousel</a></li>
+                                                <li><a href="advisor-2-colum.html">Advisor Two Colum</a></li>
+                                                <li><a href="advisor-3-colum.html">Advisor Three Colum</a></li>
+                                                <li><a href="advisor-carousel-2.html">Advisor Carousel Two</a></li>
+                                            </ul>
+                                        </div>
+                                    </div><!-- end col-3 -->
+                                    <div class="col-menu col-md-3">
+                                        <h6 class="title">User Pages</h6>
+                                        <div class="content">
+                                            <ul class="menu-col">
+                                                <li><a href="profile.html">Profile</a></li>
+                                                <li><a href="edit-profile.html">Edit Profile</a></li>
+                                                <li><a href="login.html">login</a></li>
+                                                <li><a href="register.html">register</a></li>
+                                            </ul>
+                                        </div>
+                                    </div><!-- end col-3 -->
+                                    <div class="col-menu col-md-3">
+                                        <h6 class="title">Other Pages</h6>
+                                        <div class="content">
+                                            <ul class="menu-col">
+                                                <li><a href="about-us.html">About Us</a></li>
+                                                <li><a href="faq.html">Faq</a></li>
+                                                <li><a href="pricing-table.html">Pricing Table</a></li>
+                                                <li><a href="contact.html">Contact</a></li>
+                                                <li><a href="404.html">Error Page</a></li>
+                                            </ul>
+                                        </div>
+                                    </div><!-- end col-3 -->
+                                </div><!-- end row -->
+                            </li>
+                        </ul>
+                    </li>
+                    <li class="dropdown">
+                        <a href="#" class="dropdown-toggle active" data-toggle="dropdown">Courses</a>
+                        <ul class="dropdown-menu">
+                            <li><a href="courses.html">Courses Carousel One</a></li>
+                            <li><a href="courses-2.html">Courses Grid One</a></li>
+                            <li><a href="courses-3.html">Courses Grid Two</a></li>
+                            <li><a href="courses-4.html">Courses Carousel Two</a></li>
+                            <li><a href="course-details.html">Course Details</a></li>
+                        </ul>
+                    </li>
+                    <li class="dropdown">
+                        <a href="#" class="dropdown-toggle active" data-toggle="dropdown">Teachers</a>
+                        <ul class="dropdown-menu">
+                            <li><a href="teachers.html">Advisor</a></li>
+                            <li><a href="teachers-details.html">Advisor Details</a></li>
+                        </ul>
+                    </li>
+                    <li class="dropdown">
+                        <a href="#" class="dropdown-toggle active" data-toggle="dropdown">Event</a>
+                        <ul class="dropdown-menu">
+                            <li><a href="event.html">Event Mixed Colum</a></li>
+                            <li><a href="event-2.html">Event Grid Colum</a></li>
+                            <li><a href="event-3.html">Event Carousel</a></li>
+                        </ul>
+                    </li>
+                    <li class="dropdown">
+                        <a href="#" class="dropdown-toggle" data-toggle="dropdown">Blog</a>
+                        <ul class="dropdown-menu">
+                            <li><a href="blog-standard.html">Blog Standard</a></li>
+                            <li><a href="blog-left-sidebar.html">Blog Left Sidebar</a></li>
+                            <li><a href="blog-right-sidebar.html">Blog Right Sidebar</a></li>
+                            <li><a href="blog-single-standard.html">Single Standard</a></li>
+                            <li><a href="blog-single-left-sidebar.html">Single Left Sidebar</a></li>
+                            <li><a href="blog-single-right-sidebar.html">Single Right Sidebar</a></li>
+                        </ul>
+                    </li>
+                    <li>
+                        <a href="contact.html">contact</a>
+                    </li>
+                </ul>
+            </div><!-- /.navbar-collapse -->
+        </div>
+
+    </nav>
+    <!-- End Navigation -->
+
+</header>
+<!-- End Header -->
